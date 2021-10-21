b3843595feba039e0001415e0be3fc9b6f588071
IgnacioCava <cavaignacio11@gmail.com>
2021-10-21 07:24:12 -0300

    Finished the extra assignments, but found a possible evaluation error for the sortArray function

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -10,6 +10,9 @@ function deObjetoAmatriz(objeto){
       C: 3
     }) ➞ [["D", 1], ["B", 2], ["C", 3]]*/
   //Escribe tu código aquí
+
+  var array=Object.entries(objeto);
+  return array;
 }
 
 
@@ -18,6 +21,19 @@ function numberOfCharacters(string) {
   //en formato par clave-valor.
   //Ej: Recibe ---> "adsjfdsfsfjsdjfhacabcsbajda" || Devuelve ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 } 
   //Escribe tu código aquí
+  var obj={};
+  for(var i=0;i<string.length;i++){
+    if(obj[string.charAt(i)]===undefined){
+      obj[string.charAt(i)]=1;
+    }
+    else{
+      obj[string.charAt(i)]=obj[string.charAt(i)]+1;
+    }
+  }
+  
+
+  return obj;
+  
 }
 
 
@@ -26,6 +42,17 @@ function capToFront(s) {
   //al principio de la palabra.
   //Ejemplo: soyHENRY -> HENRYsoy
   //Escribe tu código aquí
+  var cap="";
+  var min="";
+  for(var i=0;i<s.length;i++){
+    if(s.charAt(i)===s.charAt(i).toUpperCase()){
+      cap=cap+s.charAt(i);
+    }
+    if(s.charAt(i)===s.charAt(i).toLowerCase()){
+      min=min+s.charAt(i);
+    }
+  }
+  return cap+min;
 }
 
 
@@ -35,6 +62,14 @@ function asAmirror(str) {
   //pero con cada una de sus palabras invertidas, como si fuera un espejo.
   //Ej: Recibe ---> "The Henry Challenge is close!" || Devuelve ---> "ehT yrneH egnellahC si !esolc"
   //Escribe tu código aquí
+  var partes=[];
+  partes=str.split(' ');/*Almacena cada palabra individual del string en un array*/
+
+  for(var P=0;P<partes.length;P++){
+    partes[P]=partes[P].split('').reverse().join('');/*Almacena cada LETRA individual de cada palabra string en un array, invierte el array y luego reuno las letras en un string*/
+  }
+  
+  return partes.join(' ');/*Reuno todas las palabras, separandolas con un espacio simple*/
 } 
 
 
@@ -43,6 +78,20 @@ function capicua(numero){
   //La misma debe retornar: "Es capicua" si el número se número que se lee igual de 
   //izquierda a derecha que de derecha a izquierda. Caso contrario retorna "No es capicua"
   //Escribe tu código aquí
+  var valor=numero.toString();
+  var largo=Math.floor(valor.length/2);
+  var cuenta=0;
+  for(var i=0;i<largo;i++){
+    if(valor.charAt(i)!==valor.charAt(valor.length-i-1)){
+        return 'No es capicua';
+    }
+    else{
+      ++cuenta;
+    }
+    if(cuenta===largo){
+      return 'Es capicua';
+    }
+  }
 }
 
 
@@ -50,6 +99,21 @@ function deleteAbc(cadena){
   //Define una función que elimine las letras "a", "b" y "c" de la cadena dada 
   //y devuelva la versión modificada o la misma cadena, en caso de contener dichas letras.
   //Escribe tu código aquí
+  var partes=[];
+  partes=cadena.split('');
+  for(var P=0;P<partes.length;P++){
+    if(partes[P]==='a'){
+    partes.splice(P, 1);
+    }
+    if(partes[P]==='b'){
+      partes.splice(P, 1);
+    }
+    if(partes[P]==='c'){
+      partes.splice(P, 1);
+    }
+  }
+  //Si no indicaba esos ifs de forma individual, la igualdad no reconocía a la 'b'('a'||'b'||'c') y splice no la borraba, no se por qué.
+  return partes.join('');
 }
 
 
@@ -57,6 +121,22 @@ function sortArray(arr) {
   //La función recibe una matriz de strings. Ordena la matriz en orden creciente de longitudes de cadena
   //Ej: Recibe ---> ["You", "are", "beautiful", "looking"] || Devuelve ---> [“You", "are", "looking", "beautiful"]
   //Escribe tu código aquí
+  var temp;
+    for(var i=0;i<arr.length;i++){
+      for(var j=0;j<arr.length;j++){
+        if(arr[i].toLowerCase().length<arr[j].toLowerCase().length){
+          temp=arr[j];
+          arr[j]=arr[i];
+          arr[i]=temp;
+      }//Se espera que 'You' esté primero en el array y 'are' segundo, obligatoriamente, si bien ambos tienen misma .length
+    }
+    
+  }
+  /*temp=arr[0];
+  arr[0]=arr[1];
+  arr[1]=temp;*/
+  //Esto pasa en ambos casos de JSX.test.js, donde de espera que 'pera' esté si o si antes que 'papa', si bien miden lo mismo
+  return arr;
 }
 
 
@@ -65,7 +145,19 @@ function buscoInterseccion(arreglo1, arreglo2){
   //retornar un nuevo array con la intersección de ambos elementos. (Ej: [4,2,3] unión [1,3,4] = [3,4].
   //Si no tienen elementos en común, retornar un arreglo vacío.
   //Aclaración: los arreglos no necesariamente tienen la misma longitud
-  //Escribe tu código aquí  
+  //Escribe tu código aquí
+  var cont=0;
+  var interseccion=[];
+    for(var i=0;i<arreglo1.length;i++){
+      for(var j=0;j<arreglo2.length;j++){
+        if(arreglo1[i]===arreglo2[j]){
+          interseccion[cont]=arreglo1[i];
+          ++cont;
+        }
+      }
+    }
+  
+  return interseccion;
 }
 
 
